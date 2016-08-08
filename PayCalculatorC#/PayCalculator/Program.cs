d9e5ea866d82ad92f827585bf5377e0b678b73b2
nlthorne <nlthorne@wi.rr.com>
2016-08-08 15:45:37 -0500

    C# fan project upload

---
+++ PayCalculatorC#/PayCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal hours;
+            decimal wages;
+            decimal result;
+
+            Console.WriteLine("Enter Hours: ");
+            string hoursEntered = Console.ReadLine();
+            Console.WriteLine("Enter Hourly Wages: ");
+            string hourWages = Console.ReadLine();
+            
+            hours = Decimal.Parse(hoursEntered);
+            wages = Decimal.Parse(hourWages);
+            result = hours * wages;
+            Console.WriteLine("Your pay is: " + result);
+            Console.ReadLine();
+            
+            
+
+
+
+
+        }
+    }
+}
