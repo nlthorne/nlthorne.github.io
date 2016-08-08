0f4101dbd09a4c70fd676affc7ce1de358adf253
nlthorne <nlthorne@wi.rr.com>
2016-08-08 18:16:14 -0500

    Fuzzbuzz upload to site

---
+++ Fizzbuzz/Fizzbuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int iNumber = 0;
+            while (iNumber < 100)
+            {
+                iNumber++;
+                if (iNumber % 3 == 0 && iNumber % 5 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (iNumber % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (iNumber % 3 == 0)
+                {
+                    Console.WriteLine("Fizzbuzz");
+                }
+                else
+                {
+                    Console.WriteLine(iNumber);
+                }
+            }
+                    Console.Read();
+        }
+    }
+}
+
